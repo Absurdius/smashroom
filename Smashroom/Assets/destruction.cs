049df39413ba9f92fd8a6dd64114bd9d324718a3
Absurdius <e94doclrd@hotmail.com>
2017-04-02 21:11:27 +0200

    My first commit

---
+++ Smashroom/Assets/destruction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destruction : MonoBehaviour {
+
+	Component[] parts; 
+	
+	// Use this for initialization
+	void Start () {
+		parts = transform.GetComponentsInChildren(typeof(Transform), true);  
+	}
+	
+	void OnCollisionEnter (Collision coll) {
+		if(coll.collider.gameObject.tag == "Weapon"){
+			//print(coll.name);
+			GetComponent<Collider>().enabled = false; 
+			foreach(Transform t in parts){
+				t.gameObject.SetActive(true);
+				t.SetParent(null);
+				t.gameObject.AddComponent<Rigidbody>();
+				t.gameObject.GetComponent<Rigidbody>().AddForce(coll.impulse); 
+				print("new cube");
+			}
+		transform.DetachChildren(); 
+		Destroy(gameObject); 
+		}
+	}
+}
