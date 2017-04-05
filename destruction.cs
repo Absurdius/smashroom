c65f3f3013bdbceac4320a84c87b27c064d8bdee
Absurdius <e94doclrd@hotmail.com>
2017-04-05 16:03:22 +0200

    improved destruction scipts.

---
+++ destruction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destruction : MonoBehaviour {
+
+	Component[] parts; 
+	
+	public double hardness; //threshold for breaking an object, nagnitude of impulse vector must be >hardness to break object
+	
+	
+	// Use this for initialization
+	void Start () {
+		parts = transform.GetComponentsInChildren(typeof(Transform), true);  
+	}
+	
+	void OnCollisionEnter (Collision coll) {
+		print(coll.impulse);
+		if(coll.collider.gameObject.tag == "Weapon" && coll.impulse.magnitude > hardness){
+			//print(coll.name);
+			GetComponent<Collider>().enabled = false; 
+			foreach(Transform t in parts){
+				t.gameObject.SetActive(true);
+				t.SetParent(null);
+				t.gameObject.AddComponent<Rigidbody>();
+				t.gameObject.GetComponent<Rigidbody>().AddForce(coll.impulse);
+                t.gameObject.AddComponent<timedestruction>();
+			}
+		transform.DetachChildren(); 
+		Destroy(gameObject); 
+		}
+	}
+}
---
+++ timedestruction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timedestruction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 2.5f);	
+	}
+}
